425ffe93343b8ce9890ce060ab060e7767a3fc99
Skyler Baker <skyler.moede17@gmail.com>
2020-08-02 17:51:35 -0400

    add ivy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ rm -rf public/*
 cargo run content.json
 mv index.html public
 cp -r images public/
-cp syles.css public
+cp styles.css public
 cp index.js public
 cp -r fancybox public/
 firebase deploy
