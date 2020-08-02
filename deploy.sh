e7da94918f1c1e8b92e6fd25c9e4cf84b4c1c44d
CalebLBaker <calebbaker774@gmail.com>
2020-08-02 10:26:08 -0400

    Update deploy script to copy javascript

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ rm -rf public/*
 cargo run content.json
 mv index.html public
 cp -r images public/
-cp syles.css publi
+cp syles.css public
+cp index.js public
+cp -r fancybox public/
 firebase deploy
 
