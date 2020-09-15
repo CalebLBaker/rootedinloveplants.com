3d0e1619456a876bcfa37367f71a4510f4717ff6
CalebLBaker <calebbaker774@gmail.com>
2020-09-15 12:11:47 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcloud builds submit --tag gcr.io/rootedinloveplants/rootedinloveplants
+gcloud builds submit --tag gcr.io/rootedinloveplants/rootedinloveplants --timeout=20m && \
 gcloud run deploy --image gcr.io/rootedinloveplants/rootedinloveplants --platform managed --port 8080
 
